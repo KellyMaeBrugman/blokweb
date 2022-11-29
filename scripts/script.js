2b2e162fa69fa9ebaff9f91771e844c9ea367df5
KellyMaeBrugman <kellybrugman@gmail.com>
2022-11-29 14:39:44 +0100

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var section = document.querySelectorAll('section');
+var video = document.querySelector('video');
+
+
+
+section.forEach(item => 
+    item.addEventListener('click', () => {
+    video.play();
+}));
