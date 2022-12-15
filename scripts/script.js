dfad258544d1e2c43a1dae797b69881323318ca6
KellyMaeBrugman <kellybrugman@gmail.com>
2022-12-15 12:37:43 +0100

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
-var videos = document.querySelectorAll('video'); // verwijst naar alle video's in de html
 
+
+/* ************************** */
+/* Menu openen en sluiten */
+/* Geholpen door Bahaa Salaymeh */
+/*************************** */
+
+var menu_icon = document.querySelector('header button'); // verwijst naar de button in de header
+var menu = document.querySelector('header nav'); // verwijst naar de nav in de header
+
+menu_icon.addEventListener('click', openmenu); // opent het menu wanneer je erop klikt
+
+function openmenu(){
+    menu.classList.toggle('menu-open'); // zorgt ervoor dat hij open en dicht kan met dezelfde button
+}
+
+/* ************************** */
+/* Video's af laten spelen en op pauze kunnen zetten */
+/* Geholpen door Nina Vens */
+/*************************** */
+
+var videos = document.querySelectorAll('video'); // verwijst naar alle video's in de html
 // loopje met functie voor elk video element
 
 videos.forEach(video =>  
@@ -13,14 +33,3 @@ videos.forEach(video =>
         }
     } 
 ));
-
-// voor het navigatie menu
-
-var menu_icon = document.querySelector('header button'); // verwijst naar de button in de header
-var menu = document.querySelector('header nav'); // verwijst naar de nav in de header
-
-menu_icon.addEventListener('click', openmenu); // opent het menu wanneer je erop klikt
-
-function openmenu(){
-    menu.classList.toggle('menu-open'); // zorgt ervoor dat hij open en dicht kan met dezelfde button
-}
